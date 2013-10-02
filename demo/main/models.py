ae3517ed7c2f7d9d296f2d68d4b04274c8e9dcc5
Iacopo Spalletti <i.spalletti@nephila.it>
2013-10-02 15:15:39 +0200

    Rework models. Remove ckeditor introspection.

--- demo/main/models.py
+++ demo/main/models.py
@@ -3,7 +3,8 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 from cms.models import CMSPlugin
-from cms.models.fields import PlaceholderField
+from filer.fields.file import FilerFileField
+from filer.fields.image import FilerImageField
 from djangocms_text_ckeditor.fields import HTMLField
 from djangocms_text_ckeditor.models import AbstractText
 
@@ -12,11 +13,12 @@ class TextModel(CMSPlugin):
     title = models.CharField(max_length=100)
     body = HTMLField(blank=True)
 
+
 class TextModel2(AbstractText):
     title = models.CharField(max_length=100)
 
-try:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ['^djangocms_text_ckeditor\.fields\.HTMLField'])
-except ImportError:
-    pass
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    logo = FilerImageField(null=True, blank=True, related_name='logo')
+    disclaimer = FilerFileField(null=True, blank=True)
--- demo/news/models.py
+++ demo/news/models.py
@@ -4,9 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 from cms.models import CMSPlugin
-from django.db import models
-from filer.fields.file import FilerFileField
-from filer.fields.image import FilerImageField
 from cms.models.fields import PlaceholderField
 from djangocms_text_ckeditor.fields import HTMLField
 
@@ -25,17 +22,5 @@ class News(models.Model):
         return reverse('newsdetail', kwargs={'slug': self.slug})
 
 
-class Company(models.Model):
-    name = models.CharField(max_length=255)
-    logo = FilerImageField(null=True, blank=True, related_name='logo')
-    disclaimer = FilerFileField(null=True, blank=True)
-
-
 class NewsPlugin(CMSPlugin):
     items = models.IntegerField(default=3)
-
-try:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ['^djangocms_text_ckeditor\.fields\.HTMLField'])
-except ImportError:
-    pass

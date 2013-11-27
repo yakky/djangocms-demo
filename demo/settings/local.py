ef451848b9f95b61c5b1f2b4312b5cb543174213
Iacopo Spalletti <i.spalletti@nephila.it>
2013-11-27 12:12:14 +0100

    Re-add local.py

---
+++ demo/settings/local.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .general import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'demo.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'ADMINUSER': 'admin',
+    },
+}
